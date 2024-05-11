a6fa142b844fec46adc21ac1c5b640744005a461
Easycoding-art <vorpvnbvoron@gmail.com>
2024-05-12 02:29:14 +0300

    attempt to create package

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup( 
 	name='db_toolkit', 
 	version='0.1', 
 	description='Useful package to create database', 
 	author='Easycoding-Art',
-    packages=find_packages(where='db_toolkit'),
-    package_dir={'': 'db_toolkit'}, 
+    packages=['db_toolkit'],
 	install_requires=[ 
 		'psycopg2', 
 		'pandas',
