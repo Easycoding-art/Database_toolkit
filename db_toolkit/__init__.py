f498da4784692135b37a68b9e75a9c48f431c485
Easycoding-art <vorpvnbvoron@gmail.com>
2024-05-12 02:57:37 +0300

    attmempt 3

--- db_toolkit/__init__.py
+++
@@ -1,3 +0,0 @@
-from db_toolkit.fake_data import *
-from db_toolkit.db import DB_Creator
-NAME = 'DATABASE_TOOLKIT'
--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import setup
-setup( 
-	name='db_toolkit', 
+from setuptools import setup, find_packages
+setup(
+    name='db_toolkit', 
 	version='0.1', 
 	description='Useful package to create database', 
 	author='Easycoding-Art',
-    packages=['db_toolkit']
-    '''
+    packages=find_packages()
 	install_requires=[ 
 		'psycopg2', 
 		'pandas',
         'faker',
-        'wikipedia',
+        'wikipedia'
 	],
-    '''
+    py_modules=["parser_file"]
 ) 
